cb6a5adbb2e3717e82d212a806ac13f01c065bdd
Hernan Antonio Martin Castillo <hernan.castillo@worldstream.com>
2024-01-19 15:49:23 +0100

    first commit

---
+++ src/main/java/com/lenzencheckapi/LenzencheckAPI/LenzencheckApiApplication.java
@@ -0,0 +1,13 @@
+package com.lenzencheckapi.LenzencheckAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenzencheckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenzencheckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lenzencheckapi/LenzencheckAPI/LenzencheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenzencheckapi.LenzencheckAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenzencheckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
