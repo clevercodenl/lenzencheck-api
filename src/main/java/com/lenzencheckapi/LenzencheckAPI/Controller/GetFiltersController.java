084c5cbaac3db5395fd623d264b8e03b598c4449
Hernan Antonio Martin Castillo <hernan.castillo@worldstream.com>
2024-01-19 17:29:04 +0100

    chore: filters endpoint

---
+++ src/main/java/com/lenzencheckapi/LenzencheckAPI/Controller/GetFiltersController.java
@@ -0,0 +1,34 @@
+package com.lenzencheckapi.LenzencheckAPI.Controller;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/")
+public class GetFiltersController {
+    private static final String FILTERS_ENDPOINT = "/filters";
+    private static final String FILTERS_FILENAME = "static/filters.json";
+
+    @GetMapping(FILTERS_ENDPOINT)
+    @ResponseBody
+    public ResponseEntity<String> getProducts() {
+        try {
+            ClassPathResource resource = new ClassPathResource(FILTERS_FILENAME);
+            Path path = Paths.get(resource.getURI());
+            String jsonData = Files.readString(path);
+
+            return ResponseEntity.ok(jsonData);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Error reading JSON file");
+        }
+    }
+}
